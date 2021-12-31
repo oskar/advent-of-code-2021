dc69d16c9fb66ee1e8ea7df019634aeb39c2c6de
Oskar Hermansson <oskar.hermansson@tradera.com>
2021-12-31 20:45:16 +0100

    day7

---
+++ day7/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var crabs = File.ReadAllText("input.txt").Split(",").Select(int.Parse).ToList();
+
+var minFuel = crabs.Min(i => crabs.Sum(j => Math.Abs(j - i)));
+Console.WriteLine("Min fuel: " + minFuel);
