c342a6d5574127dbda08d391dbf5743732c68bfc
Oskar Hermansson <oskar.hermansson@tradera.com>
2021-12-31 15:48:10 +0100

    day6

---
+++ day6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var fishes = File.ReadAllText("input.txt").Split(",").Select(int.Parse).ToList();
+Print(0, fishes);
+
+for (var iteration = 1; iteration <= 80; iteration++)
+{
+    var count = fishes.Count;
+    for (var i = 0; i < count; i++)
+    {
+        if (fishes[i] < 1)
+        {
+            fishes[i] = 6;
+            fishes.Add(8);
+        }
+        else
+        {
+            fishes[i]--;            
+        }
+    }
+    Console.WriteLine($"Iteration {iteration}, fish count {fishes.Count}");
+}
+
+Console.WriteLine("Number of fishes: " + fishes.Count);
+
+static void Print(int iteration, IEnumerable<int> fishes) 
+    => Console.WriteLine($"After {iteration} days: {string.Join(",", fishes)}");
